8b6917e3a3eb8f648de932aef5e55b6ef2961256
Erben Mo <erbenmo@gmail.com>
2012-09-03 13:30:59 -0400

    change data storage

--- shopping/shopping.cpp
+++ shopping/shopping.cpp
@@ -5,21 +5,15 @@
 
 using namespace std;
 
-struct ele{
-  ele(int _id, int _num) {id=_id, num=_num;}
-  int id, num;
-};
+int num_offer, num_products;
+int need[5];
 
-int costs[7776];
-vector<vector<ele> >ofs;
-vector<int> ofs_price;
+int ofs_num[99+5][5];
+int ofs_price[99+5];
 
 int products = 0;
 map<int, int> c2index;
 
-int num_products;
-int need[5];
-int id_price[5];
 
 int step[5] = {6*6*6*6, 6*6*6, 6*6, 6, 1}; // base 6
 
@@ -43,24 +37,17 @@ void decode(int res, int data[5]) {
 }
 
 void print() {
-  for(int i=0; i<ofs.size(); i++) {
-    for(int j=0; j<ofs[i].size(); j++) {
-      cout << ofs[i][j].id << " * " << ofs[i][j].num << " ";
+  for(int i=0; i<num_offer + num_products; i++) {
+    for(int j=0; j<num_products; j++) {
+      cout << ofs_num[i][j] << " ";
     }
     cout << " with price: " << ofs_price[i] << endl;
   }
-
-  cout << "# products: " << num_products << endl;
-  for(int i=0; i<num_products; i++)
-    cout << "for " << i << " needs " << need[i] << " price: " << id_price[i] << endl;
 }
 
 int main() {
-  int num_offer;
-
   cin>>num_offer;
   for(int i=0; i<num_offer; i++) {
-    vector<ele> of;
     int of_size, price;
     
     cin>>of_size;
@@ -72,29 +59,52 @@ int main() {
 	c2index[c] = products++;
 
       index = c2index[c];
-      of.push_back(ele(index, k));      
+      assert(index < 5);
+      ofs_num[i][index] = k;
     }
 
-    ofs.push_back(of);
-    
     cin>>price;
-    ofs_price.push_back(price); 
+    ofs_price[i] = price; 
   }
 
   
-  cin>>num_products;  
+  cin>>num_products;
   for(int i=0; i<num_products; i++) {
-    int c, num, price, index;
-    cin>>c>>num>>price;
+    int c, k, price, index;
+    
+    cin>>c>>k>>price;
     index = c2index[c];
 
     assert(c2index.find(c) != c2index.end());
     assert(index <= 4 && index >= 0);
-    assert(id_price[index] == 0);
+    assert(ofs_price[num_offer + index] == 0);
 
-    need[index] = num;
-    id_price[index] = price;
+    need[index] = k;
+    
+    ofs_num[num_offer + index][index] = 1;
+    ofs_price[num_offer + index] = price;
   }
 
+  print();
   
+  /*
+  // finally start DP
+  int hash[7776];
+  
+  for(int i=0; i<ofs.size(); i++) {
+    for(int s0=0; s0<=needs[0]; s0++)
+      for(int s1=0; s1<=needs[1]; s1++)
+	for(int s2=0; s2<=needs[2]; s2++)
+	  for(int s3=0; s3<=needs[3]; s3++)
+	    for(int s4=0; s4<=needs[4]; s4++) {
+	      int bag[5];
+	      bag[0]=s0, bag[1]=s1, bag[2]=s2, bag[3]=s3, bag[4]=s4;	      
+	      int bag_id = encode(bag);
+
+	      
+	    }
+  }
+
+  int final_bag_id = encode(need);
+  cout << hash[final_bag_id];*/
 }
