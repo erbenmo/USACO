d16a405202b2bb5cc1ae2bec99106406efb79e47
Erben Mo <erbenmo@gmail.com>
2012-09-05 18:19:32 -0400

    refine

--- game1/game.cpp
+++ game1/game.cpp
@@ -13,19 +13,8 @@ ofstream fout("game1.out");
 
 int f[100][100];
 int sum[100][100];
-bool choice[100][100]; // T:Left - F:Right
 int v[100];
 int N;
-
-void print() {
-  for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      cout << f[i][j] << " ";
-    }
-    cout << endl;
-  }
-  cout << "-----\n";
-}
 
 int main() {
   cin>>N;
@@ -38,26 +27,12 @@ int main() {
 
   for(int l=0; l<N; l++) {
     for(int i=0; i+l<N; i++) {
-      if(l == 0) {
+      if(l == 0)
 	f[i][i] = v[i];
-	choice[i][i] = true;
-      } else if(l == 1) {
-	f[i][i+1] = max(v[i], v[i+1]);
-	choice[i][i+1] = (v[i] >= v[i+1]);
-      } else {
-	int lv, rv, j = i+l;
-
-	// for lv
-	if(choice[i+1][j]) lv = v[i] + f[i+2][j];
-	else lv = v[i] + f[i+1][j-1];
-
-	// for rv
-	if(choice[i][j-1]) rv = v[j] + f[i+1][j-1];
-	else rv = v[j] + f[i][j-2];
-
-	f[i][j] = max(lv, rv);
-	choice[i][j] = (lv >= rv);
-      }
+      int lv, rv, j = i+l;
+      lv = v[i] + sum[i+1][j]-f[i+1][j];
+      rv = v[j] + sum[i][j-1]-f[i][j-1];
+      f[i][j] = max(lv, rv);
     }
   }
 
