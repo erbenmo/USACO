6c1945a47810d70e9e16ec111b1b090c7e3834ab
Erben Mo <erbenmo@gmail.com>
2012-09-09 03:22:13 -0400

    done yhe

--- fence4/fence.cpp
+++ fence4/fence.cpp
@@ -1,6 +1,20 @@
+/*
+PROB: fence4
+LANG: C++
+ */
 #include <iostream>
+#include <fstream>
 #include <cmath>
 using namespace std;
+
+ifstream fin("fence4.in");
+ofstream fout("fence4.out");
+
+//#define cin fin
+//#define cout fout
+
+const double eps = 0.00001;
+const double eps2 = 0.000002;
 
 struct v {
   v(double _x, double _y) {x=_x, y=_y;}
@@ -8,15 +22,34 @@ struct v {
 };
 
 double cross(v A, v B) { return A.x*B.y - A.y*B.x; }
-bool same_side(v from, v to, v c, v d) { // on-line -> not same side
-  return cross(v(to.x-from.x, to.y-from.y), v(c.x-from.x, c.y-from.y))
-    *cross(v(to.x-from.x, to.y-from.y), v(d.x-from.x, d.y-from.y)) > 0;
+
+bool close(bool from, bool to) {
+  return abs(abs(from) - to) < eps;
 }
+
+bool parallel(v A, v B, v C, v D) {
+  v AB(B.x-A.x, B.y-A.y), CD(D.x-C.x, D.y-C.y);
+  return close(cross(AB, CD), 0);
+}
+
+bool same_side(v from, v to, v c, v d) {
+  double ab_ac = cross(v(to.x-from.x, to.y-from.y), v(c.x-from.x, c.y-from.y));
+  double ab_ad = cross(v(to.x-from.x, to.y-from.y), v(d.x-from.x, d.y-from.y));
+
+  if((ab_ac > 0 && ab_ad > 0) || (ab_ac < 0 && ab_ad < 0))
+    return true;
+  if((ab_ac > 0 && ab_ad < 0) || (ab_ac < 0 && ab_ad > 0))
+    return false;
+
+  if(close(ab_ac, 0) && close(ab_ad, 0))
+    return true;
+  
+  return false;
+}
+
 bool intersect(v A, v B, v C, v D) {
   return !same_side(C, D, A, B)  && !same_side(A, B, C, D);
 }
-
-const double eps = 0.000001;
 
 int N;
 double points[200][2];
@@ -37,11 +70,8 @@ void ray(double dir_x, double dir_y) {
     Cx = points[i][0], Cy = points[i][1];
     Dx = points[j][0], Dy = points[j][1];
 
-    // parallel
-    v AB(Bx-Ax, By-Ay), CD(Dx-Cx, Dy-Cy);
-    if(abs(cross(AB, CD)) < eps) {
+    if(parallel(v(Ax, Ay), v(Bx, By), v(Cx, Cy), v(Dx, Dy)))
       continue;
-    }
 
     // find dist from A->B
     double dist_1, dist_2;
@@ -98,7 +128,7 @@ int main() {
       v L(points[l][0], points[l][1]);
 
       if(intersect(I, J, K, L)) {
-	//cout << i << j << k << l << endl;
+	//cout << i << " " << j << " " << k << " " << l << endl;
 	//cout << "collide, " << I.x << I.y << J.x << J.y << K.x << K.y << L.x << L.y << endl;
 	cout << "NOFENCE\n";
 	return 0;
@@ -109,8 +139,8 @@ int main() {
   //cout << "fence valid!\n";
 
   for(int i=0; i<N; i++) {
-    ray(points[i][0], points[i][1]+eps);
-    ray(points[i][0], points[i][1]-eps);
+    ray(points[i][0]-eps, points[i][1]+eps);
+    ray(points[i][0]+eps, points[i][1]-eps);
   }
 
   int result_num = 0;
@@ -126,12 +156,16 @@ int main() {
   }
   
   if(result_num >1) {
-    if(results[result_num-1][0] < results[result_num-2][1]) {
+    int a = results[result_num-2][0], b=results[result_num-2][1];
+    int c = results[result_num-1][0], d = results[result_num-1][1];
+    
+    if(d<b || (d==b && c<a)) {
       swap(results[result_num-1][0], results[result_num-2][0]);
       swap(results[result_num-1][1], results[result_num-2][1]);
     }
   }
 
+  cout << result_num << endl;
   for(int i=0; i<result_num; i++) {
     int a = results[i][0], b = results[i][1];
     cout << points[a][0] << " " << points[a][1] << " " << points[b][0] << " " << points[b][1] << endl;
